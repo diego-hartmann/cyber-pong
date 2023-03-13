8858a2777922dc8cd11444c7ad4efc53b0f41d68
diego-hartmann <dtghartmann@gmail.com>
2023-03-13 17:33:41 -0300

    smooth game loop

--- js/logic.js
+++ js/logic.js
@@ -26,66 +26,125 @@ const line = {
     }
 }
 
-const player1 = {
+const racket1 = {
     w:15,
     h:200,
     draw(){
         // racket
         canvasCtx.fillStyle = "#ffffff";
         canvasCtx.fillRect( 10, 400, this.w, this.h );
-            // score
-        canvasCtx.font = "bold 72px Arial";
-        canvasCtx.textAlign = "center";
-        canvasCtx.textBaseline = "top";
-        canvasCtx.fillStyle = "#01341D";
-        canvasCtx.fillText('3', field.getWidth()/4, 50)
-    } 
+    }
 }
 
-const player2 = {
-    w:15,
-    h:200,
+const player1 = {
+    racket:{
+        w:15,
+        h:200,
+        y: 200,
+        draw(){
+            canvasCtx.fillStyle = "#ffffff";
+            canvasCtx.fillRect( 10, this.y, this.w, this.h );
+        }
+    },
+    score:{
+        value: 0,
+        draw(){
+            canvasCtx.font = "bold 72px Arial";
+            canvasCtx.textAlign = "center";
+            canvasCtx.textBaseline = "top";
+            canvasCtx.fillStyle = "#01341D";
+            canvasCtx.fillText(this.value.toString(), field.getWidth()/4, 50)
+        }
+    },
     draw(){
-        // racket
-        canvasCtx.fillStyle = "#ffffff";
-        canvasCtx.fillRect(
-            field.getWidth() - 15 - 10, 200, this.w, this.h );
-        // score
-        canvasCtx.font = "bold 72px Arial";
-        canvasCtx.textAlign = "center";
-        canvasCtx.textBaseline = "top";
-        canvasCtx.fillStyle = "#01341D";
-        canvasCtx.fillText('1', field.getWidth()/4 + field.getWidth()/2, 50)
+        this.racket.draw();
+        this.score.draw();
+    } 
+
+}
+const player2 = {
+    racket:{
+        w:15,
+        h:200,
+        y: 200,
+        draw(){
+            canvasCtx.fillStyle = "#ffffff";
+            canvasCtx.fillRect( field.getWidth() - 15 - 10, this.y, this.w, this.h );
+        }
+    },
+    score: {
+        value: 0,
+        draw(){
+            canvasCtx.font = "bold 72px Arial";
+            canvasCtx.textAlign = "center";
+            canvasCtx.textBaseline = "top";
+            canvasCtx.fillStyle = "#01341D";
+            canvasCtx.fillText(this.value.toString(), field.getWidth()/4 + field.getWidth()/2, 50)
+        }
+    },
+    draw(){
+        this.racket.draw();
+        this.score.draw();
     }
 }
 
 const ball = {
+    x: 300,
+    y: 200,
+    r: 20,
+    speed: 5,
     draw(){
         canvasCtx.fillStyle = "#ffffff";
         canvasCtx.beginPath();
-        canvasCtx.arc(200, 300, 20, 0, (2*Math.PI), false);
+        canvasCtx.arc(this.x, this.y, this.r, 0, (Math.PI*2), false);
         canvasCtx.fill();
+        this.move(1,0);
+    },
+    move(x, y){
+        this.x += x * this.speed;
+        this.y += y * this.speed;
     }
 }
 
 const game = {
     draw(){
-        this.checkSize();
+        this.setup();
         field.draw();
         line.draw();
         player1.draw();
         player2.draw();
         ball.draw();
     },
-    checkSize(){
+    setup(){
         canvas.width = canvasCtx.width = field.getWidth();
         canvas.height = canvasCtx.height = field.getHeight();
     }
 }
 
-game.draw();
-// when the user resizes the window,
-window.onresize = () => {
-    // the canvas must rerender the context
-    game.draw();
-}
+// smooth frame change
+window.animateFrame = (()=>{
+    return(
+        // native
+        window.requestAnimationFrame ||
+        // google
+        window.webkitRequestAnimationFrame || 
+        // mozilla
+        window.mozRequestAnimationFrame || 
+        // opera
+        window.oRequestAnimationFrame || 
+        // microsoft
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        }
+    )
+})();
+
+
+
+game.setup(); // first setup
+
+(function main(){ // creating and calling the init function
+    animateFrame(main); // loop the init function (recursive)
+    game.draw(); // draw each frame
+})()
